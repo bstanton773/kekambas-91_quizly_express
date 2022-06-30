f3df58010c0fa7b310fc659a5d34d56cab750d1d
Brian <bstanton773@gmail.com>
2022-06-30 11:20:12 -0500

    Setup createQuiz mutation

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,5 +1,6 @@
-const { GraphQLString } = require('graphql');
-const { User } = require('../models');
+const { GraphQLString, GraphQLNonNull, GraphQLList } = require('graphql');
+const { QuestionInputType } = require('./types')
+const { User, Quiz, Question } = require('../models');
 
 
 const register = {
@@ -43,4 +44,61 @@ const login = {
     }
 }
 
-module.exports = { register, login };
+
+const createQuiz = {
+    type: GraphQLString,
+    args: {
+        questions: {
+            type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(QuestionInputType)))
+        },
+        title: {
+            type: GraphQLString
+        },
+        description: {
+            type: GraphQLString
+        },
+        userId: {
+            type: GraphQLString
+        }
+    },
+    async resolve(parent, args){
+        // Generate the slug to quiz for url
+        let slugify = args.title.toLowerCase().replace(/[^\w ]+/g, '').replace(/ +/g, '-');
+        let fullSlug;
+        
+        /* Add a random interger to the end of the slug, check that the slug doesn't exist
+        If it does exist, generate a new slug, otherwise continue on */
+        while (true){
+            let slugId = Math.floor(Math.random()*10000);
+            fullSlug = `${slugify}-${slugId}`;
+
+            const existingQuiz = await Quiz.findOne({ slug: fullSlug });
+
+            if (!existingQuiz){break}
+        }
+
+        const quiz = new Quiz({
+            title: args.title,
+            slug: fullSlug,
+            description: args.description,
+            userId: args.userId
+        })
+
+        await quiz.save();
+
+        // Loop through the questions argument and create questions and connect to new quiz
+        for (const question of args.questions){
+            const questionItem = new Question({
+                title: question.title,
+                correctAnswer: question.correctAnswer,
+                order: Number(question.order),
+                quizId: quiz.id
+            })
+            questionItem.save()
+        }
+
+        return quiz.slug
+    }
+}
+
+module.exports = { register, login, createQuiz };
