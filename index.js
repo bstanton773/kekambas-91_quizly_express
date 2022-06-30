300953fc466d49d8e27f95983d2d5b04ad4da561
Brian <bstanton773@gmail.com>
2022-06-30 12:07:32 -0500

    Install ejs, set up view engine and views folder

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path')
 const { connectDB } = require('./src/db');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./src/graphql/schema')
@@ -15,9 +16,19 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }))
 
+// Set the view engine
+app.set('view engine', 'ejs');
+// update location of views folder that res.render uses
+app.set('views', path.join(__dirname, '/src/templates/views'))
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
+
+
+app.get('/test', (req, res) => {
+    res.render('test')
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server now running on PORT ${process.env.PORT}`)
