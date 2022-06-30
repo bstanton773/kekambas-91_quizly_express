66e8a6bb930eb824a602984df64b2ebf4925266f
Brian <bstanton773@gmail.com>
2022-06-30 15:02:41 -0500

    Add axios and make post request to GraphQL server to create new user

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -1,5 +1,7 @@
+const axios = require('axios');
+
+
 module.exports = async (req, res) => {
-    console.log(req.body);
     if (req.body.password !== req.body.confirmPass){
         res.send({ error: "Your passwords do not match "})
         return
@@ -8,6 +10,26 @@ module.exports = async (req, res) => {
     const mutation = `mutation register($email: String!, $username: String!, $password: String!) {
         register(email: $email, username: $username, password: $password)
     }`
-    
-    res.send('Registered!');
+    try {
+        const { data } = await axios.post('http://localhost:3000/graphql', 
+            {
+                query: mutation,
+                variables: {
+                    email: req.body.email,
+                    password: req.body.password,
+                    username: req.body.username
+                }
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+        console.log(data)
+        res.redirect('/');
+    } catch(e){
+        console.log(e)
+        res.redirect('/auth/register')
+    }
 }
