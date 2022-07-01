c4bb6f36951116018e35ec193b5eb4ca3ba3423b
Brian <bstanton773@gmail.com>
2022-07-01 12:07:54 -0500

    Add logic for Post method to /create to create quiz

--- src/routes/quiz/index.js
+++ src/routes/quiz/index.js
@@ -2,5 +2,7 @@ const QuizDashboardRouter = require('express').Router();
 
 QuizDashboardRouter.route('/create')
     .get(require('./editor.js'))
+    .post(require('./create.js'))
+
 
 module.exports = QuizDashboardRouter
