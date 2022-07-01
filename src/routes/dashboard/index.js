f56d31292efd7c6ef0225c2a33b98d65befc1f1f
Brian <bstanton773@gmail.com>
2022-07-01 11:05:09 -0500

    Add submissions

--- src/routes/dashboard/index.js
+++ src/routes/dashboard/index.js
@@ -5,4 +5,8 @@ MainDashboardRouter.route('/')
     .get(require('./dashboard.view.js'))
 
 
+MainDashboardRouter.route('/submissions')
+    .get(require('./submissions.view.js'))
+
+
 module.exports = MainDashboardRouter;
